d03d1514122fb0fab038644ac15a13add4e796e3
Xpystum <qjq3@mail.ru>
2025-01-21 17:39:16 +0300

    добавлен redis в docker-compose.yml + указаны доп установки в DockerFile для работы phpredis в laravel

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,11 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class TestController extends Controller
 {
     public function test()
     {
-        dd(8);
+        dd(1);
+        if(Cache::get('key'))
+        {
+            dd(Cache::get('key'));
+
+        } else {
+
+            Cache::set('key', 5);
+
+        }
+
+        return Cache::get('key');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,7 @@ use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [TestController::class , 'test']);
+
+// Route::get('/', function () {
+//     return route('welcome');
+// });
